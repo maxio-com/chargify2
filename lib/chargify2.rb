f4cf16644c40e1aefc5ed1fdd61bf3066e053c66
Shay Frendt <shay.frendt@gmail.com>
2011-08-25 09:48:39 -0400

    Checking in an example file load order that raises the following error:
    .../chargify2/lib/chargify2/resource.rb:12:in `<class:Resource>': uninitialized constant Chargify2::Client

--- lib/chargify2.rb
+++ lib/chargify2.rb
@@ -3,6 +3,11 @@ require 'addressable/uri'
 require 'hashie'
 require 'hashery/opencascade'
 
-Dir["#{File.dirname(__FILE__)}/chargify2/**/*.rb"].each {|f| require f}
+require 'chargify2/utils'
+require 'chargify2/direct'
+require 'chargify2/resource'
+require 'chargify2/client'
+require 'chargify2/representations/call'
+require 'chargify2/resources/call_resource'
 
 Hash.send(:include, Chargify2::Utils::HashExtensions)
