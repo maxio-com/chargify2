54c8700fad61c4dead04066048c7c11edc4673be
Matthew Johnston <warmwaffles@gmail.com>
2012-09-09 00:17:40 -0500

    Hashery classes updated

--- lib/chargify2/representations/call.rb
+++ lib/chargify2/representations/call.rb
@@ -7,24 +7,24 @@ module Chargify2
     property :success
     property :request
     property :response
-    
+
     def request
       Request.new(self[:request] || {})
     end
-    
+
     def response
       Response.new(self[:response] || {})
     end
-    
+
     def successful?
       response.result.status_code.to_s == '200'
     end
-    
+
     def errors
       (response.result.errors || []).map {|e| OpenCascade.new(e.symbolize_keys)}
     end
-    
-    class Request < OpenCascade; end
-    class Response < OpenCascade; end
+
+    class Request < Hashery::OpenCascade; end
+    class Response < Hashery::OpenCascade; end
   end
 end
