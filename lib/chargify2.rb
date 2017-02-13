ae86cf14a744202452cdcd688b65413c8f3c284b
Wendy Smoak <wsmoak@gmail.com>
2017-02-13 14:21:57 -0500

    Add new files to library

--- lib/chargify2.rb
+++ lib/chargify2.rb
@@ -31,6 +31,7 @@ require 'chargify2/representations/renewal_preview'
 require 'chargify2/representations/metadatum'
 require 'chargify2/representations/reason_codes'
 require 'chargify2/representations/reason_code'
+require 'chargify2/representations/hold'
 
 # resources
 require 'chargify2/resources/call_resource'
@@ -45,3 +46,4 @@ require 'chargify2/resources/allocation_preview_resource'
 require 'chargify2/resources/renewal_preview_resource'
 require 'chargify2/resources/metadatum_resource'
 require 'chargify2/resources/reason_code_resource'
+require 'chargify2/resources/hold_resource'
