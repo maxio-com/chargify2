c4e6873863a5c183c7e700eb8a8377e6ce692332
Przemysław Haptaś <przemyslawhaptas@gmail.com>
2019-05-16 16:07:19 -0500

    Change MigrationResource to accept params as one hash

--- lib/chargify2/resources/migration_resource.rb
+++ lib/chargify2/resources/migration_resource.rb
@@ -1,6 +1,5 @@
 module Chargify2
   class MigrationResource < Resource
-
     def self.path
       'migrations'
     end
@@ -9,16 +8,16 @@ module Chargify2
       Migration
     end
 
-    def preview(subscription_id, plan_attrs = {}, options = {})
-      self.class.preview(subscription_id, plan_attrs, merge_options(options))
+    def preview(migration_params, options = {})
+      self.class.preview(migration_params, merge_options(options))
     end
 
-    def self.preview(subscription_id, plan_attrs, options = {})
-      options.merge!(body: { migration: { subscription_id: subscription_id }.merge!(plan_attrs) }.to_json)
+    def self.preview(migration_params, options = {})
+      options.merge!(body: { migration: migration_params }.to_json)
       response = post("/#{path}/preview", options)
       response_hash = response[representation.to_s.downcase.split('::').last] || {}
 
-      self.create_response(
+      create_response(
         representation.new(response_hash.symbolize_keys),
         response['meta']
       )
--- spec/resources/offer_resource_spec.rb
+++ spec/resources/offer_resource_spec.rb
@@ -2,6 +2,12 @@ require 'spec_helper'
 
 module Chargify2
   describe OfferResource do
-    pending
+    it 'has the correct path' do
+      expect(described_class.path).to eql('offers')
+    end
+
+    it 'represents the Offer resource' do
+      expect(described_class.representation).to eql(Offer)
+    end
   end
 end
